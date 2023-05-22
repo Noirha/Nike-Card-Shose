5cc1dc994d6f7a3316da5e2f47208f105bae2bae
norhan abas <122152347+Noirha@users.noreply.github.com>
2023-05-22 00:02:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,117 @@
+let one = document.querySelector(".one")
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four")
+let blueShose = document.querySelector(".blue-shose");
+
+one.addEventListener("click", (eo) => {
+    blueShose.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P4.jpg"
+})
+
+two.addEventListener("click", (eo) => {
+    blueShose.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P5.jpg"
+})
+
+three.addEventListener("click", (eo) => {
+    blueShose.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P3.jpg"
+})
+
+
+four.addEventListener("click", (eo) => {
+    blueShose.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P7.jpg"
+})
+
+
+let show = document.querySelector(".show");
+let box = document.querySelector(".box");
+
+show.addEventListener("click", (eo) => {
+    box.style.transform = "rotateY(180deg)"
+})
+
+let next = document.querySelector(".but-back");
+
+
+
+
+next.addEventListener("click", (eo) => {
+    box.style.transform = "rotateY(360deg)"
+})
+
+
+
+let img = document.querySelector("#first-img");
+let blue = document.querySelector("#blue");
+let green = document.querySelector("#green");
+let orange = document.querySelector("#orange");
+let back = document.querySelector(".back");
+
+blue.addEventListener("click", (eo) => {
+    img.src = "imges/2021_Nike_PG_5_EP_Black_White_Royal_Blue_CW3146-503.jpg"
+    blueShose.src = "imges/Nike_Air_Foamposite_Pro_USA_Blue_Void_Gum_Light_Brown_White_CJ0325-400_P2.jpg"
+    one.src = "imges/Nike_Air_Foamposite_Pro_USA_Blue_Void_Gum_Light_Brown_White_CJ0325-400_P4.jpg"
+    one.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_Air_Foamposite_Pro_USA_Blue_Void_Gum_Light_Brown_White_CJ0325-400_P4.jpg"
+    })
+    two.src = "imges/Nike_Air_Foamposite_Pro_USA_Blue_Void_Gum_Light_Brown_White_CJ0325-400_P5.jpg"
+    two.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_Air_Foamposite_Pro_USA_Blue_Void_Gum_Light_Brown_White_CJ0325-400_P5.jpg"
+    })
+    three.src = "imges/Nike_Air_Foamposite_Pro_USA_Blue_Void_Gum_Light_Brown_White_CJ0325-400_P3.jpg"
+    three.addEventListener("click", (eo) => {
+        blueShose.src = " imges/Nike_Air_Foamposite_Pro_USA_Blue_Void_Gum_Light_Brown_White_CJ0325-400_P3.jpg"
+    })
+    four.src = "imges/Nike_Air_Foamposite_Pro_USA_Blue_Void_Gum_Light_Brown_White_CJ0325-400_P1.jpg";
+    four.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_Air_Foamposite_Pro_USA_Blue_Void_Gum_Light_Brown_White_CJ0325-400_P1.jpg"
+    })
+
+})
+
+green.addEventListener("click", (eo) => {
+    img.src = "imges/Nike_PG_5_EP_Play_for_the_Future_Green_Glow_Glacier_Blue_Platinum_Tint_CW3146-300.jpg";
+    blueShose.src = "imges/Nike_Air_Zoom_Alphafly_NEXT_2_Mint_Foam_Coconut_Milk_Cave_Purple_DV9422-900_P2.jpg"
+    one.src = "imges/Nike_Air_Zoom_Alphafly_NEXT_2_Mint_Foam_Coconut_Milk_Cave_Purple_DV9422-900_P4.jpg";
+    one.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_Air_Zoom_Alphafly_NEXT_2_Mint_Foam_Coconut_Milk_Cave_Purple_DV9422-900_P4.jpg";
+
+
+    })
+
+    two.src = "imges/Nike_Air_Zoom_Alphafly_NEXT_2_Mint_Foam_Coconut_Milk_Cave_Purple_DV9422-900_P5.jpg";
+    two.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_Air_Zoom_Alphafly_NEXT_2_Mint_Foam_Coconut_Milk_Cave_Purple_DV9422-900_P5.jpg"
+    })
+    three.src = "imges/Nike_Air_Zoom_Alphafly_NEXT_2_Mint_Foam_Coconut_Milk_Cave_Purple_DV9422-900_P3.jpg";
+    three.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_Air_Zoom_Alphafly_NEXT_2_Mint_Foam_Coconut_Milk_Cave_Purple_DV9422-900_P3.jpg"
+    })
+    four.src = "imges/Nike_Air_Zoom_Alphafly_NEXT_2_Mint_Foam_Coconut_Milk_Cave_Purple_DV9422-900_P1.jpg";
+    four.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_Air_Zoom_Alphafly_NEXT_2_Mint_Foam_Coconut_Milk_Cave_Purple_DV9422-900_P1.jpg";
+    })
+
+})
+
+
+orange.addEventListener("click", (eo) => {
+    img.src = "imges/2021_Nike_PG_5_EP_Black_Bright_Crimson_Multi_Color_CW3146-505.jpg";
+    blueShose.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P2.jpg"
+    one.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P4.jpg";
+    one.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P4.jpg"
+    })
+    two.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P5.jpg";
+    two.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P5.jpg"
+    })
+    three.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P3.jpg";
+    three.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P3.jpg"
+    })
+    four.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P7.jpg"
+    four.addEventListener("click", (eo) => {
+        blueShose.src = "imges/Nike_ACG_Mountain_Fly_Low_SE_Canyon_Purple_Amethyst_Wave_Doll_DQ1979-500_P7.jpg"
+    })
+
+})
